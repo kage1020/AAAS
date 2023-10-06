2c12fa19e074713848a34862519e19ef5b8ceb74
kage1020 <4620087@ed.tus.ac.jp>
2023-10-07 08:58:35 +0900

    add tailwind and react-player

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,28 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { Card, CardBody } from '@nextui-org/react';
+import ReactPlayer from 'react-player';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'Annotation App for Action Segmentation' },
+    { name: 'description', content: 'Welcome to AAAS!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className='bg-gray-200 dark:bg-gray-950 h-screen p-4 grid grid-cols-12 grid-rows-6 gap-2'>
+      <Card className='bg-white dark:bg-gray-800 col-span-6 row-span-6'>
+        <CardBody>editor</CardBody>
+      </Card>
+      <Card className='bg-white dark:bg-gray-800 col-span-6 row-span-4'>
+        <CardBody>
+          <ReactPlayer url={['https://www.youtube.com/watch?v=LXb3EKWsInQ']} />
+        </CardBody>
+      </Card>
+      <Card className='bg-white dark:bg-gray-800 col-span-6 row-span-2'>
+        <CardBody>controller</CardBody>
+      </Card>
+    </main>
   );
 }
